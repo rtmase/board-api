5481ecdded253c2f0218fdb2330b991e0d1566b4
LDH <rtmase@gamil.com>
2026-06-17 10:57:05 +0900

    board-api initial commit
    - spring boot 3.5.15
    - spring security, JWT, Redis, MySQL
    - swagger
    - docker

---
+++ src/test/java/com/rtmase/board/BoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtmase.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
